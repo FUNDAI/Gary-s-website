9445ca5c333e99097f23439cfdbe46fd6bd6c64a
FUNDAI <96116846+FUNDAI@users.noreply.github.com>
2023-01-07 20:02:21 +0800

    BRUH

---
+++ script.js
@@ -0,0 +1,9 @@
+function Ins(){
+    window.location.href='https://www.instagram.com/frc7130_future_shock/'
+}
+function FB(){
+    window.location.href='https://www.facebook.com/fablabMDHSfrc7130'
+}
+function YT(){
+    window.location.href='https://www.youtube.com/channel/UCrr_gyEFzddEHeuWRg1fU7Q/featured'
+}
